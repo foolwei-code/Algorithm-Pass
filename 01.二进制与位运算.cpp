a565e2fd4831bac6e3926072afd568161ff46fc9
konger <konger@1234.com>
2025-10-20 19:33:22 +0800

    item01

--- 01.二进制与位运算.cpp
+++ 01.二进制与位运算.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
 using namespace std;
+//practice1 : 打印int型数据的二进制状态
+class Solution
+{
+public:
+	//左侧为高位，右侧为低位
+	void PrintBinary(int nums)
+	{
+		for (int i = 31; i >= 0; i--)
+		{
+			cout << (((nums>>i) & 1) == 1) ? 1 : 0;
+		}
+	}
+};
 int main()
 {
-	cout << "hello" << endl;
+	Solution p;
+	int num;
+	cin >> num;
+	p.PrintBinary(num);
 	return 0;
 }

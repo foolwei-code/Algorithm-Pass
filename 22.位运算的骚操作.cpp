138ddfcc71f4c3ebba3ed15494f824f1070af496
konger <konger@1234.com>
2025-11-05 20:13:01 +0800

    commit

--- 22.位运算的骚操作.cpp
+++ 22.位运算的骚操作.cpp
@@ -28,6 +28,39 @@ public:
         return  1162261467 % n == 0;
     }
 };
+// 3.已知n是非负数
+// 返回大于等于n的最小的2某次方
+// 如果int范围内不存在这样的数，返回整数最小值
+class Solution3
+{
+public:
+    int near2power(int n)
+    {
+        if (n <= 0)
+            return  1;
+        n--;
+        n |= (n >> 1);
+        n |= (n >> 2);
+        n |= (n >> 4);
+        n |= (n >> 8);
+        n |= (n >> 16);
+        return ++n;
+    }
+};
+// 4.给你两个整数 left 和 right ，表示区间 [left, right]
+// 返回此区间内所有数字 & 的结果
+// 包含 left 、right 端点
+// 测试链接 : https://leetcode.cn/problems/bitwise-and-of-numbers-range/
+class Solution4 {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        while (left < right)
+        {
+            right -= (right & -right);
+        }
+        return right;
+    }
+};
 int main()
 {
 	return 0;

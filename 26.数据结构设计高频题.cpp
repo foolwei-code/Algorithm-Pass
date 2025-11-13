baa1ab1e0a9455f4724a7fffdceeea76cab75ddf
konger <konger@1234.com>
2025-11-13 23:22:38 +0800

    commit

--- 26.数据结构设计高频题.cpp
+++ 26.数据结构设计高频题.cpp
@@ -2,12 +2,114 @@
 #include<vector>
 #include<unordered_map>
 #include<utility>
+#include<random>
+#include<ctime>
 using namespace std;
 // 1.setAll功能的哈希表
 // 测试链接 : https://www.nowcoder.com/practice/7c4559f138e74ceb9ba57d76fd169967
 unordered_map<int, pair<int,int>>map;
 int n, opt,x,y,Time;
 int settime, setvalue; //时间戳技术
+//2.实现LRU结构
+// 测试链接 : https://leetcode.cn/problems/lru-cache/
+class LRUCache {
+private:
+    struct ListNode {
+        int key;
+        int value;
+        ListNode* prev;
+        ListNode* next;
+        ListNode(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+    };
+    unordered_map<int, ListNode*> map;
+    ListNode* head;
+    ListNode* tail;
+    int capacity;
+    int size;
+
+    void removeNode(ListNode* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void addToTail(ListNode* node) {
+        node->prev = tail->prev;
+        node->next = tail;
+        tail->prev->next = node;
+        tail->prev = node;
+    }
+
+    void moveToTail(ListNode* node) {
+        removeNode(node);
+        addToTail(node);
+    }
+
+public:
+    LRUCache(int cap) : capacity(cap), size(0) {
+        head = new ListNode(-1, -1);
+        tail = new ListNode(-1, -1);
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if (map.find(key) == map.end()) return -1;
+        ListNode* node = map[key];
+        moveToTail(node);
+        return node->value;
+    }
+
+    void put(int key, int value) {
+        if (map.find(key) != map.end()) {
+            ListNode* node = map[key];
+            node->value = value;
+            moveToTail(node);
+        }
+        else {
+            if (size == capacity) {
+                ListNode* toDelete = head->next;
+                removeNode(toDelete);
+                map.erase(toDelete->key);
+                delete toDelete;
+                size--;
+            }
+            ListNode* newNode = new ListNode(key, value);
+            addToTail(newNode);
+            map[key] = newNode;
+            size++;
+        }
+    }
+};
+//3. 插入、删除和获取随机元素O(1)时间的结构
+// 测试链接 : https://leetcode.cn/problems/insert-delete-getrandom-o1/
+class RandomizedSet {
+public:
+    RandomizedSet() {
+        size = 0;
+    }
+
+    bool insert(int val) {
+        if (map.count(val))
+            return false;
+        arr.push_back(val);
+        map[val] = size++;
+        return true;
+    }
+
+    bool remove(int val) {
+
+    }
+
+    int getRandom() {
+
+    }
+private:
+    vector<int>arr;
+    unordered_map<int, int>map;
+    int size;
+};
+
+
 int main()
 {
 	cin >> n;
---
+++ 27.二叉树高频题目1.cpp
@@ -0,0 +1,156 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+// 1.二叉树的层序遍历
+// 测试链接 : https://leetcode.cn/problems/binary-tree-level-order-traversal/
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+	
+};
+class Solution1 {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>>ans;
+		if (root == nullptr)
+			return ans;
+		vector<TreeNode*>queue(2000);
+		int l = 0, r = 0;
+		queue[r++] = root;
+		while (l < r)
+		{
+			int size = r - l;
+			vector<int>temp;
+			for (int i = 0; i < size; i++)
+			{
+				TreeNode* front = queue[l++];
+				temp.push_back(front->val);
+				if (front->left != nullptr)
+					queue[r++] = front->left;
+				if (front->right != nullptr)
+					queue[r++] = front->right;
+			}
+			ans.push_back(temp);
+		}
+		return ans;
+	}
+
+};
+// 2.二叉树的锯齿形层序遍历
+// 测试链接 : https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal/
+class Solution2 {
+public:
+	vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+		vector<vector<int>>ans;
+		if (root == nullptr)
+			return ans;
+		vector<TreeNode*>queue(2000);
+		int l = 0, r = 0;
+		bool reverse = false;
+		queue[r++] = root;
+		while (l < r)
+		{
+			int size = r - l;
+			vector<int>temp;
+			//将进入队列与收集元素分离
+			for (int i = (reverse ? r - 1 : l), k = 0, j = (reverse ? -1 : 1); k < size; i += j,k++)
+			{
+				temp.push_back(queue[i]->val);
+			}
+			for (int i = 0; i < size; i++)
+			{
+				TreeNode* front = queue[l++];
+				if (front->left != nullptr)
+					queue[r++] = front->left;
+				if (front->right != nullptr)
+					queue[r++] = front->right;
+			}
+			ans.push_back(temp);
+			reverse = !reverse;
+		}
+
+		return ans;
+	}
+};
+// 3.二叉树的最大特殊宽度
+// 测试链接 : https://leetcode.cn/problems/maximum-width-of-binary-tree/
+class Solution3 {
+public:
+	int widthOfBinaryTree(TreeNode* root) {
+		if (root == nullptr)
+			return 0;
+		//按照层序遍历的方式进行遍历
+		//维护两个队列，一个维护节点，另一个维护节点的编号
+		vector<TreeNode*>queue1(3000);
+		vector<unsigned long long>queue2(3000);
+		unsigned long long l1 = 0, r1 = 0, l2 = 0, r2 = 0,ans=0;
+		//头节点进入队列
+		queue1[r1++] = root;
+		queue2[r2++] = 1;
+		while (l1 < r1)
+		{
+			int size = r1 - l1;
+			ans = max(ans, queue2[r2 - 1] - queue2[l2]+1);
+			for (int i = 0; i < size; i++)
+			{
+				TreeNode* front = queue1[l1++];
+				unsigned long long index = queue2[l2++];
+				if (front->left != nullptr)
+				{
+					queue1[r1++] = front->left;
+					queue2[r2++] = 2 * index;
+				}
+				if (front->right != nullptr)
+				{
+					queue1[r1++] = front->right;
+					queue2[r2++] = 2 * index+1;
+				}
+			}
+
+		}
+		return ans;
+	}
+};
+// 4.求二叉树的最大、最小深度
+// 测试链接 : https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+//求二叉树的最大深度，也就是高度
+class Solution4 {
+public:
+	int maxDepth(TreeNode* root) {
+		if (root == nullptr)
+			return 0;
+		return f(root);
+	}
+	int f(TreeNode* root)
+	{
+		return root == nullptr ? 0 : max(f(root->left), f(root->right)) + 1;
+	}
+};
+//5.求二叉树的最小深度(必须到叶节点）
+class Solution5 {
+public:
+	int minDepth(TreeNode* root) {
+		if (root == nullptr)
+			return 0;
+		return f(root);
+	}
+	int f(TreeNode* root)
+	{
+		if (root->left == nullptr && root->right == nullptr)
+			return  1;
+		int leftdepth = 0x7fffffff, rightdepth = 0x7fffffff;
+		if (root->left != nullptr);
+		leftdepth = f(root->left);
+		if (root->right != nullptr)
+			rightdepth = f(root->right);
+		return min(leftdepth, rightdepth) + 1;
+	}
+};
+int main()
+{
+	return 0;
+}

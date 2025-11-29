6d1ff0d3892ed61534ab1e34841e437bc957f216
konger <konger@1234.com>
2025-11-29 19:04:42 +0800

    commit

--- 30.嵌套递归的实现.cpp
+++ 30.嵌套递归的实现.cpp
@@ -90,4 +90,49 @@ public:
 private:
 	int where;
 };
+// 2.含有嵌套的字符串解码
+// 测试链接 : https://leetcode.cn/problems/decode-string/
+class Solution2 {
+public:
+	string decodeString(string s) {
+		where = 0;
+		return f(0, s);
+	}
+	string f(int k, string& s)
+	{
+		int cur = 0;
+		int i = k;
+		string ans;
+		while (i < s.size() && s[i] != ']')
+		{
+			if (s[i] >= '0' && s[i] <= '9')
+				cur = cur * 10 + (s[i++] - '0');
+			else if (s[i] == '[')
+			{
+				string temp = f(i + 1, s);
+				for (int i = 0; i < cur; i++)
+					ans += temp;
+				i = where+1;
+				cur = 0;
+			}
+			else
+			{
+				ans.push_back(s[i++]);
+			}
+		}
+		where = i;
+		return ans;
+	}
+private:
+	int where;
+};
+// 3.含有嵌套的分子式求原子数量
+// 测试链接 : https://leetcode.cn/problems/number-of-atoms/
+class Solution3 {
+public:
+	string countOfAtoms(string formula) {
+
+	}
+};
+
 
